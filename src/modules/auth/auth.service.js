e55716d7d1b4541d5945d3be09d8c5ee15d2a755
Mr.T <84755654+TerryRangel@users.noreply.github.com>
2026-06-01 11:43:25 -0600

    Update Nodemailer configuration for Gmail

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -109,11 +109,13 @@ export class AuthService {
 
     // 5. Configurar Nodemailer
     const transporter = nodemailer.createTransport({
-      service: 'gmail', 
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
+        pass: process.env.EMAIL_PASS,
+      },
     });
 
     // 6. Enviar correo (usamos FRONTEND_URL del .env o localhost por defecto)
